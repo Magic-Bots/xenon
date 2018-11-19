9cc89fc611b89b66ec7d0617de5a793b0d447736
Merlintor <merlinfuchs2001@gmail.com>
2018-11-19 17:33:48 +0100

    Asciidoc highlighting

--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,6 @@
 from discord.ext import commands as cmd
+import inspect
+import itertools
 
 
 class HelpFormatter(cmd.HelpFormatter):
@@ -44,6 +46,64 @@ class HelpFormatter(cmd.HelpFormatter):
         cmd = self.command
 
         return prefix + self._signature(cmd)
+
+    async def format(self):
+        """
+        Added asciidoc highlighting
+        """
+        self._paginator = cmd.formatter.Paginator(prefix="```asciidoc")
+
+        # we need a padding of ~80 or so
+
+        description = self.command.description if not self.is_cog() else inspect.getdoc(self.command)
+
+        if description:
+            # <description> portion
+            self._paginator.add_line(description, empty=True)
+
+        if isinstance(self.command, cmd.Command):
+            # <signature portion>
+            signature = self.get_command_signature()
+            self._paginator.add_line(signature, empty=True)
+
+            # <long doc> section
+            if self.command.help:
+                self._paginator.add_line(self.command.help, empty=True)
+
+            # end it here if it's just a regular command
+            if not self.has_subcommands():
+                self._paginator.close_page()
+                return self._paginator.pages
+
+        max_width = self.max_name_size
+
+        def category(tup):
+            cog = tup[1].cog_name
+            # we insert the zero width space there to give it approximate
+            # last place sorting position.
+            return cog + ':' if cog is not None else '\u200bNo Category:'
+
+        filtered = await self.filter_command_list()
+        if self.is_bot():
+            data = sorted(filtered, key=category)
+            for category, commands in itertools.groupby(data, key=category):
+                # there simply is no prettier way of doing this.
+                commands = sorted(commands)
+                if len(commands) > 0:
+                    self._paginator.add_line(category)
+
+                self._add_subcommands_to_page(max_width, commands)
+        else:
+            filtered = sorted(filtered)
+            if filtered:
+                self._paginator.add_line('Commands:')
+                self._add_subcommands_to_page(max_width, filtered)
+
+        # add the ending note
+        self._paginator.add_line()
+        ending_note = self.get_ending_note()
+        self._paginator.add_line(ending_note)
+        return self._paginator.pages
 
 
 def setup(bot):
